54b6bcdc3f34ca029bb6801c703e6072e25ee28e
Ash Arya <ashutosharyan@gmail.com>
2020-12-07 18:40:40 -0500

    Added a failing integration test

---
+++ CSharp/Specs/ConfigMgr.IntegrationSpecs/JsonConfigReaderSpecs.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace ConfigMgr.IntegrationSpecs
+{
+    [TestFixture]
+    public class JsonConfigReaderSpecs
+    {
+        [Test]
+        public void Should_read_app_setting_values()
+        {
+            Assert.Fail("Test failed xyz");
+        }
+    }
+}
